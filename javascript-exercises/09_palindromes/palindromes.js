621b89d3d0ed9c4596fcbac42d451a4a8d156ae6
Floris <floriset@gmail.com>
2022-08-26 12:44:15 +0200

    Update palindromes.js

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    stringLess = string.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    stringLess = stringLess.replace(/\s+/g, '')
+    let array = stringLess.split('');
+    console.log(stringLess)
+    let revAr = array.reverse();
+    let revStr = revAr.join('');
+    console.log(revStr)
+    if (revStr == stringLess) {
+        console.log(' its a palindrome')
+        return true;
+    } else { 'no palindrome'
+            return false;}
 
 };
 
