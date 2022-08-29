90544944dcfb04f9a638e212546d7ce3a1e64ab1
flos06 <floriset@gmail.com>
2022-08-29 17:19:37 +0200

    complete calculator but still have to add decimal possibilty and potential cleanup

--- calculator/myscript.js
+++ calculator/myscript.js
@@ -6,7 +6,13 @@
     let result = 0;
 
     function store(val){
-
+        if (operator == '='){
+            outputL.innerHTML = ''
+            currentVal = 0
+            prevV = 0
+            operator = ''
+            result = 0;
+        }
         outputL.innerHTML += val.innerHTML;
         currentVal = outputL.innerHTML;
         console.log(currentVal)
@@ -34,8 +40,58 @@ function add() {
         outputL.innerHTML = ''
         operator = '+'
         console.log(prevV)
+
     }
 
+
+}
+
+function multiply() {
+    if (result != 0) {
+        prevV = result
+        currentVal = 0
+        outputL.innerHTML = ''
+        operator = 'x'
+    } else {
+        prevV = currentVal
+        currentVal = 0
+        outputL.innerHTML = ''
+        operator = 'x'
+
+    }
+
+
+}
+function divide() {
+    if (result != 0) {
+        prevV = result
+        currentVal = 0
+        outputL.innerHTML = ''
+        operator = '/'
+    } else {
+        prevV = currentVal
+        currentVal = 0
+        outputL.innerHTML = ''
+        operator = '/'
+
+    }
+
+
+}
+
+function substract() {
+    if (result != 0) {
+        prevV = result
+        currentVal = 0
+        outputL.innerHTML = ''
+        operator = '-'
+    } else {
+        prevV = currentVal
+        currentVal = 0
+        outputL.innerHTML = ''
+        operator = '-'
+
+    }
 
 }
 
@@ -43,5 +99,23 @@ function calculate() {
     if (operator == '+'){
         result = Number(currentVal) + Number(prevV)
         outputL.innerHTML = result
+        outputUpper.innerHTML = prevV + ' + ' + currentVal + ' = ' + result
+    } else if (operator == 'x'){
+        result = Number(currentVal) * Number(prevV)
+        outputL.innerHTML = result
+        outputUpper.innerHTML = prevV + ' x ' + currentVal + ' = ' + result
+    } else if (operator == '/'){
+        result = Number(currentVal) / Number(prevV)
+        result = Math.round(result * 100) / 100
+        outputL.innerHTML = result
+        outputUpper.innerHTML = prevV + ' / ' + currentVal + ' = ' + result
+    } else if (operator == '-') {
+        result = Number(prevV) - Number(currentVal)
+        outputL.innerHTML = result
+        outputUpper.innerHTML = prevV + ' - ' + currentVal + ' = ' + result
     }
+
+
+
+    operator = '='
 }
